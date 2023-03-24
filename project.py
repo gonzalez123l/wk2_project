4c2d250db78f36dbee5459b420b1a9730b0d1318
Wasmo101 <workifabian@gmail.com>
2023-03-24 13:02:01 -0700

    update

--- project.py
+++ project.py
@@ -15,22 +15,52 @@ while True:
     user_choice = input("Enter a choice(rock, paper, scissors):")
     choices = ['rock', 'paper', 'scissors']
     bot_choice = random.choice(choices)
+    bot_score = 0
+    user_score = 0
     print(f'You chose {user_choice} and the bot chose {bot_choice}')
-
+    
     if user_choice == bot_choice:
         print(f'You both selected {user_choice} you just tied to a bot, loser! :p')
     elif user_choice == 'rock':
         if bot_choice == 'scissors':
             print(f'Rock beats scissors! You Win! L bot lol')
+            user_score += 1
+            if user_score >= 3:
+                print("congratulations you gain a point")
+                break
         else:
-            print('Paper beats rock! You lost! You lost to a bot, loser!')
+            print('Paper beats rock! You lost! Unbelievable You lost to a bot, loser!')
+            bot_score += 1
+            if bot_score >= 3:
+                print("you loose better luck next time!")
+
+                break
     elif user_choice == 'scissors':
         if bot_choice == 'paper':
-            print(f'Rock beats scissors! You Win! L bot lol')
+            print(f'scissors beats paper! You Win! L bot lol')
+            user_score += 1
+            if user_score >= 3:
+                print("congratulations you gain a point")
+                break
+
         else:
-            print('Paper beats rock! You lost! You lost to a bot, loser!')
+            print('rock beats scissors! You lost! Hilarious You lost to a bot, loser!')
+            bot_score += 1
+            if bot_score >= 3:
+                print("you loose better luck next time!")
+                break
     elif user_choice == 'paper':
         if bot_choice == 'rock':
-            print(f'Rock beats scissors! You Win! L bot lol')
+            print(f'paper beats Rock! You Win! L bot lol')
+            user_score += 1
+            if user_score >= 3:
+                print("congratulations you gain a point")
+                break
         else:
-            print('Paper beats rock! You lost! You lost to a bot, loser!')
+            print('Paper losser to scissors! You lost! You lost to a bot, loser!')
+            bot_score += 1 
+            if bot_score >= 3:
+                print("you loose better luck next time!")
+                break
+    
+    print(f"the current score {user_score} and bot score {bot_score}")       
